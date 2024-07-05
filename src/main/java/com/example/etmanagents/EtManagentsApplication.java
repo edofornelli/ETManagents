0b9f072aa97736fe6547e96dc8555df18b264482
edofornelli <114942883+edofornelli@users.noreply.github.com>
2024-07-05 09:48:45 +0200

    Pubblicazione repo
    scheletro progetto ed aggiunta dipendenze

---
+++ src/main/java/com/example/etmanagents/EtManagentsApplication.java
@@ -0,0 +1,13 @@
+package com.example.etmanagents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtManagentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EtManagentsApplication.class, args);
+    }
+
+}
